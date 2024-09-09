2fb622843ac2ccd0aedea2dcd86da2c2cd3fa111
Paulo Jalowyj <paulo.jalowyj@outlook.com>
2024-09-09 10:31:27 -0300

    refactor: refactoring the project structure with routers and annotated

--- fast_zero/security.py
+++ fast_zero/security.py
@@ -20,7 +20,7 @@ from fast_zero.settings import Settings
 
 pwd_context = PasswordHash.recommended()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 
 def get_password_hash(password: str):
