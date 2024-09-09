f10c91c248c8a75529d441f9f45edf98a8ea357d
Paulo Jalowyj <paulo.jalowyj@outlook.com>
2024-09-09 20:48:30 -0300

    Implements todos endpoints

--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 from pydantic import BaseModel, ConfigDict, EmailStr
+
+from fast_zero.models import TodoState
 
 
 class Message(BaseModel):
@@ -25,3 +29,25 @@ class UserList(BaseModel):
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class TodoSchema(BaseModel):
+    title: str
+    description: str
+    state: TodoState
+
+
+class TodoPublic(TodoSchema):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+
+class TodoList(BaseModel):
+    todos: list[TodoPublic]
+
+
+class TodoUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    state: TodoState | None = None
